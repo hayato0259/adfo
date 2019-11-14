c63912b3eb485a8873d80a5a658d04b3aa4a5927
Hayato Asami <mail@has-on.info>
2019-11-14 18:51:23 +0900

    他のファイルの内容表示

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 def show():
-    test_data = open("templates/index2.html", "r")
+    test_data = open("templates/test.txt", "r")
     contents = test_data.read()
     test_data.close()
     return contents
 
 @app.route("/")
 def main():
-    return render_template("index.html", s=show())
+    s = show()
+    return render_template("index.html", s=s)
 
 if __name__ == "__main__":
     print('on hello')
