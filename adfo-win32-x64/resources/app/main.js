bb8237168c220d557142e761e66effafa4bd6813
Hayato Asami <mail@has-on.info>
2019-11-18 17:56:19 +0900

    add dist

---
+++ adfo-win32-x64/resources/app/main.js
@@ -0,0 +1,42 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+let mainWindow;
+
+app.on('window-all-closed', function () {
+    app.quit();
+});
+
+app.on('ready', function () {
+    var subpy = require('child_process').spawn('python', ['./resources/app/main.py']);
+    var rq = require('request-promise');
+    var mainAddr = 'http://localhost:5000';
+
+    var openWindow = function () {
+        mainWindow = new BrowserWindow({
+            frame: false,
+            width: 1024,
+            height: 640,
+            backgroundColor: '#122738',
+        });
+        mainWindow.loadURL(mainAddr);
+        // mainWindow.openDevTools();
+
+        mainWindow.on('closed', function () {
+            mainWindow = null;
+            subpy.kill('SIGINT');
+        });
+    };
+
+    var startUp = function () {
+        rq(mainAddr)
+            .then(function (htmlString) {
+                openWindow();
+            })
+            .catch(function (err) {
+                startUp();
+            });
+    };
+
+    startUp();
+});
